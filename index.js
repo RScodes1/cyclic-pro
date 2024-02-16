436f298b25039d9405873642f9fa45fd97a4a068
rajcodes168h <raj19all@gmail.com>
2024-02-17 00:36:30 +0530

    hi

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-require('dotenv').config()
-const{connection} = require('./config/db');
+require('dotenv').config();
+const { connection } = require('./config/db');
+const { userRouter } = require('./routes/user.routes');
+const { noteRouter } = require('./routes/note.routes');
+const cors = require('cors');
 
-const {userRouter} = require('./routes/user.routes');
-const {noteRouter} =require('./routes/note.routes')
+app.use(cors()); 
 
-// const {auth} = require('./middlewares/auth.middleware');
-
-app.get('/', (req,res)=>{
-    res.send({"msg": "welcome to the server"});
-})
+app.get('/', (req, res) => {
+    res.send({"msg": "hi to the server"});
+});
 
 app.use(express.json());
-app.use('/users',userRouter);
-app.use('/notes',noteRouter);
+app.use('/users', userRouter);
+app.use('/notes', noteRouter);
 
-app.listen(process.env.PORT, async()=> {
+app.listen(process.env.PORT, async () => {
     try {
-        await connection
-        console.log("hi this is mongodb");
+        await connection;
+        console.log("Connected to MongoDB");
         console.log(`The server is running on port ${process.env.PORT}`);
     } catch (error) {
-         console.log(error);
+        console.log(error);
     }
-})
+});
